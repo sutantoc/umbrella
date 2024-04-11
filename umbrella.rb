bc83070cfcb1aa62fb8dbfce69bf3d7f63f451df
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-04-11 01:25:37 +0000

    gmaps api parse

--- umbrella.rb
+++ umbrella.rb
@@ -1,4 +1,29 @@
 require "http"
 require "json"
 
+line_width = 40
 
+puts "=" * line_width
+puts "Will you need an umbrella today?"
+puts "=" * line_width
+puts "Where are you?"
+location = gets
+puts "Checking the weather at #{location}"
+
+gmaps_key = ENV["GMAPS_KEY"]
+pirate_weather_key = ENV["PIRATE_WEATHER_KEY"]
+
+gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{location}&key=#{gmaps_key}"
+
+raw_gmaps = HTTP.get(gmaps_url)
+parsed_gmaps = JSON.parse(raw_gmaps)
+results_array = parsed_gmaps.fetch("results")
+first_result_hash = results_array.at(0)
+geometry_hash = first_result_hash.fetch("geometry")
+location_hash = geometry_hash.fetch("location")
+latitude = location_hash.fetch("lat")
+longitude = location_hash.fetch("lng")
+
+puts "Your coordinates are #{latitude}, #{longitude}."
+
+# pp "It is currently XXXX"
